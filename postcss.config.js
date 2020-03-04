8f69c141149b06b7bdf69650e9f927f35fa1fee7
Josh Pigford <josh@joshpigford.com>
2020-03-04 13:41:29 -0600

    Initial frontend shell

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,40 @@
 module.exports = {
   plugins: [
-    require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
+    require("postcss-import"),
+    require("postcss-flexbugs-fixes"),
+    require("postcss-preset-env")({
       autoprefixer: {
-        flexbox: 'no-2009'
+        flexbox: "no-2009"
       },
       stage: 3
-    })
+    }),
+    require("tailwindcss")({
+      theme: {
+        container: {
+          center: true
+        },
+        borderRadius: {
+          large: "20px"
+        },
+        fontFamily: {
+          sans: [
+            "sofia-pro-soft",
+            "-apple-system",
+            "BlinkMacSystemFont",
+            "Arial",
+            "sans-serif"
+          ]
+        },
+        extend: {
+          colors: {
+            bluegray: "#cbe5ec",
+            skyblue: "#e5f4f8",
+            blue: "#6bdbff",
+            green: "#4ecd84"
+          }
+        }
+      }
+    }),
+    require("autoprefixer")
   ]
-}
+};
